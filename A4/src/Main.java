277400cb60f8d52aeb831bf5ee4a43fa34b5b72b
aimeesimons <smnaim002@myuct.ac.za>
2023-04-17 13:40:39 +0200

    Generated javadocs

--- A4/src/Main.java
+++ A4/src/Main.java
@@ -16,10 +16,24 @@ public class Main {
 
         BinarySearchTree users = new BinarySearchTree();
         Scanner input = new Scanner(System.in);
-        users.insert(new User("aimee", "student at UCT"));
+        users.insert(new User("Aimee", "student at UCT"));
         users.insert(new User("234", "CEO of being cool"));
-        users.insert(new User("erin", "tall girl"));
-        users.insert(new User("maxine", "cute girl"));
+        users.insert(new User("Erin", "tall girl"));
+        users.insert(new User("Maxine", "cute girl"));
+        users.insert(new User("MaraJade7", "single and loving it!"));
+        users.insert(new User("DarthMaul6", "CEO of being cool"));
+        users.insert(new User("DashRendar8", "tall girl"));
+        users.insert(new User("Revan9", "cute girl"));
+        users.insert(new User("C-3PO7", "Star Wars fan"));
+        users.insert(new User("Watto8", "student at UCT"));
+        users.insert(new User("KitFisto9", "CEO of being cool"));
+        users.insert(new User("HanSolo7", "Star Wars fan"));
+        users.insert(new User("Obi-WanKenobi3", "Star Wars fan"));
+        users.insert(new User("LukeSkywalker2", "this is not my actual name"));
+        users.insert(new User("AsajjVentress3", "CEO of being cool"));
+        users.insert(new User("SateleShan3", "bye"));
+        users.insert(new User("R2-D28", "hi"));
+        users.insert(new User("DarthBane3", "Star Wars fan"));
         System.out.println("-----------------------------------------------------------------");
         System.out.println("\t\t # WELCOME TO TOKTIK!! #");
         System.out.println("-----------------------------------------------------------------");
@@ -38,13 +52,14 @@ public class Main {
                     input.nextLine();
                     System.out.println("Enter the account name:");
                     String names = input.next();
-                    BinaryTreeNode search = users.UserExists(users, names);
-                    if (search == null) {
+                    boolean search = users.UserExists(users, names);
+                    if (search == false) {
                         System.out.println("Account name does not exist.");
                         System.out.println("");
 
                     } else {
-                        System.out.println("\t>The profile description is: \"" + search.data.Description + "\"");
+                        System.out.println("\t>The profile description is: \""
+                                + users.userfound(users, names).data.Description + "\"");
                         System.out.println("");
                     }
 
@@ -61,8 +76,8 @@ public class Main {
                     input.nextLine();
                     System.out.println("Enter an account name:");
                     String Name = input.next();
-                    BinaryTreeNode check = users.UserExists(users, Name);
-                    if (check == null) {
+                    boolean check = users.UserExists(users, Name);
+                    if (check == false) {
                         System.out.println("Provide a profile description:");
                         input.nextLine();
                         String description = input.nextLine();
@@ -78,8 +93,8 @@ public class Main {
                 case 4:
                     System.out.println("Enter the account name which you would like to delete:");
                     String del = input.next();
-                    BinaryTreeNode exists = users.UserExists(users, del);
-                    if (exists == null) {
+                    boolean exists = users.UserExists(users, del);
+                    if (exists == false) {
                         System.out.println("\t>Account name does not exist");
                         System.out.println("");
 
@@ -92,8 +107,8 @@ public class Main {
                 case 5:
                     System.out.println("Enter an account name:");
                     String postsName = input.next();
-                    BinaryTreeNode checking = users.UserExists(users, postsName);
-                    if (checking == null) {
+                    boolean checking = users.UserExists(users, postsName);
+                    if (checking == false) {
                         System.out.println("\t>Account name does not exit.");
                         System.out.println("");
 
@@ -102,7 +117,7 @@ public class Main {
                         temp1 = users.find(temp1).data;
                         Collections.reverse(temp1.posts);
                         System.out.println("\t-------------------------------------");
-                        System.out.println("\t|LIST OF POSTS FROM " + postsName + "\t\t|");
+                        System.out.println("\t|LIST OF POSTS FROM " + postsName + "\t      |");
                         System.out.println("\t-------------------------------------");
                         for (int i = 0; i < temp1.posts.size(); i++) {
                             System.out.println("\t" + temp1.posts.get(i).toString());
@@ -115,30 +130,37 @@ public class Main {
                     input.nextLine();
                     System.out.println("Enter an account name:");
                     String postName = input.next();
-                    User temp2 = new User(postName);
-                    temp2 = users.find(temp2).data;
-                    System.out.println("Would you like to post a video? y/n");
-                    String ans = input.next();
-                    if (ans.toUpperCase().equals("Y")) {
-                        System.out.println("Enter the video file name");
-                        String vid = input.next();
-                        System.out.println("Enter a Title");
-                        input.nextLine();
-                        String descrip = input.nextLine();
-                        temp2.posts.add(new Post(descrip, vid));
+                    boolean checkTree = users.UserExists(users, postName);
+                    if (checkTree == false) {
+                        System.out.println("\t>Account name does not exist");
+                        System.out.println("");
+
                     } else {
-                        System.out.println("Enter a comment");
-                        input.nextLine();
-                        String com = input.nextLine();
-                        temp2.posts.add(new Post(com));
+                        User temp2 = new User(postName);
+                        temp2 = users.find(temp2).data;
+                        System.out.println("Would you like to post a video? y/n");
+                        String ans = input.next();
+                        if (ans.toUpperCase().equals("Y")) {
+                            System.out.println("Enter the video file name");
+                            String vid = input.next();
+                            System.out.println("Enter a Title");
+                            input.nextLine();
+                            String descrip = input.nextLine();
+                            temp2.posts.add(new Post(descrip, vid));
+                            System.out.println("");
+                        } else {
+                            System.out.println("Enter a comment");
+                            input.nextLine();
+                            String com = input.nextLine();
+                            temp2.posts.add(new Post(com));
+                            System.out.println("");
+                        }
                     }
+
                     break;
 
                 case 7:
-                    String newString = System.getProperty("user.dir").replaceAll("\\\\", "/") + "/A4/dataset.txt";
-                    System.out.println(newString);
-                    String li = Line(
-                            "dataset.txt");
+                    String li = Line("dataset.txt");
                     int space = li.indexOf(" ");
                     if (li.substring(0, space).toUpperCase().equals("ADD")) {
                         String newSentence = li.substring(space + 1);
@@ -155,8 +177,8 @@ public class Main {
                             space = newSentence.indexOf(" ");
 
                             String descrip = newSentence.substring(space + 1);
-                            BinaryTreeNode checkNode = users.UserExists(users, username);
-                            if (checkNode == null) {
+                            boolean checkNode = users.UserExists(users, username);
+                            if (checkNode == false) {
                                 System.out.println("\t!Username does not exit and can therefore not add post!");
 
                             } else {
@@ -167,8 +189,8 @@ public class Main {
 
                         } else {
                             String descrip = newSentence.substring(space + 1);
-                            BinaryTreeNode checkNode = users.UserExists(users, username);
-                            if (checkNode == null) {
+                            boolean checkNode = users.UserExists(users, username);
+                            if (checkNode == false) {
                                 System.out.println("\t!Username does not exit and can therefore not add post!");
                             } else {
                                 User temp = users.find(new User(username)).data;
